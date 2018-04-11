ec9b9d637b4cbcb70b9637711dadd377f3ff77fb
K1kk3rt <37216006+K1kk3rt@users.noreply.github.com>
2018-04-11 21:55:24 +0200

    Add files via upload

---
+++ WisMaster_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WisMaster_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
