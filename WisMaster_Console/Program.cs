9b4fb8c593ff73f755fe855d6102f4f644a17b43
Eugenie <eugenie-heijn@hotmail.com>
2018-04-11 23:13:44 +0200

    SumL1, check, generate score not finished. Check formula

--- WisMaster_Console/Program.cs
+++ WisMaster_Console/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WisMaster_Console
 {
     class Program
     {
+        public static SumL1 sum = new SumL1();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welkom to WisMaster! Press any key to start...");
+            Console.WriteLine("Press 0 to stop");
+
+            Console.ReadKey();
+            
+            while (true)
+            {
+                GenerateSumL1();
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                if (CheckIfRightAnswer())
+                {
+                    stopwatch.Stop();
+                    TimeSpan time = stopwatch.Elapsed;
+                    GenerateScore(time);
+                }
+                Console.ReadKey();
+                
+            }
+            
+            //Console.ReadKey();
+        }
+
+        public static void GenerateSumL1()
+        {
+            //Generate random digits for sum
+            Random random = new Random();
+            sum.Digit1 = random.Next(0,9);
+            sum.Digit2 = random.Next(0, 9);
+
+            PrintSumL1();
+        }
+
+        public static void PrintSumL1()
+        {
+            Console.WriteLine(sum.Digit1.ToString() + " + " + sum.Digit2.ToString() + " =");
+        }
+
+        public static bool CheckIfRightAnswer()
+        {
+            bool answer = false;
+
+            sum.Answer = sum.Digit1 + sum.Digit2;
+
+            int userAnswer = Int32.Parse(Console.ReadLine());
+
+            if (sum.Answer == userAnswer)
+            {
+                answer = true;
+                Console.WriteLine("Right!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+            return answer;
+        }
+
+        public static void GenerateScore(TimeSpan time)
+        {
+            double scoreTime = time.TotalSeconds;
+            double score = (1/10000) * scoreTime * scoreTime;
+
+            Console.WriteLine("Score: " + score);
         }
     }
 }
---
+++ WisMaster_Console/Sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WisMaster_Console
+{
+    public struct SumL1
+    {
+        public int Digit1;
+        public int Digit2;
+        public int Answer;
+    }
+}
